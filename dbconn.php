5d010b1fff11727fbbe7a5650542bfcb4687d3e7
seflm <marek.sefl@seznam.cz>
2016-07-29 20:09:38 +0200

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,8 +1,8 @@
 <?php
     $servername = "localhost";
-    $username = "seflmar";
-    $password = "885748";
-    $dbname = "seflmar";
+    $username = "root";
+    $password = "kosticka";
+    $dbname = "kc";
     $link = @mysql_connect($servername,$username,$password) or die('Nelze se pripojit k databazi.');
     @mysql_select_db($dbname, $link) or die('Databaze neexistuje');
     mysql_query ('set names utf8'); 
